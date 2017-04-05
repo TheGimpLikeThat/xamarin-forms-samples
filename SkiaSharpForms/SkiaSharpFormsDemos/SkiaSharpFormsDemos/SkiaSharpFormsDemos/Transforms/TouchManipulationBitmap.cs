b70b0fb35cb273441ad7a301346fb70732e3819c
Charles Petzold <chape@microsoft.com>
2017-04-05 16:31:32 -0400

    [SkiaSharpForms] Touch Manipulation Demo

---
+++ SkiaSharpForms/SkiaSharpFormsDemos/SkiaSharpFormsDemos/SkiaSharpFormsDemos/Transforms/TouchManipulationBitmap.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using SkiaSharp;
+
+using TouchTracking;
+
+namespace SkiaSharpFormsDemos.Transforms
+{
+    class TouchManipulationBitmap
+    {
+        SKBitmap bitmap;
+        Dictionary<long, TouchManipulationInfo> touchDictionary = 
+            new Dictionary<long, TouchManipulationInfo>();
+
+        public TouchManipulationBitmap(SKBitmap bitmap)
+        {
+            this.bitmap = bitmap;
+            Matrix = SKMatrix.MakeIdentity();
+        }
+
+        public SKMatrix Matrix { set; get; }
+
+        public void Paint(SKCanvas canvas)
+        {
+            canvas.Save();
+            SKMatrix matrix = Matrix;
+            canvas.Concat(ref matrix);
+            canvas.DrawBitmap(bitmap, 0, 0);
+            canvas.Restore();
+        }
+
+        public bool HitTest(SKPoint location)
+        {
+            SKRect rect = new SKRect(0, 0, bitmap.Width, bitmap.Height);
+            rect = Matrix.MapRect(rect);
+            return rect.Contains(location);
+        }
+
+        public void ProcessTouchEvent(long id, TouchActionType type, SKPoint location)
+        {
+            switch (type)
+            {
+                case TouchActionType.Pressed:
+                    touchDictionary.Add(id, new TouchManipulationInfo
+                    {
+                        PreviousPoint = location,
+                        NewPoint = location
+                    });
+                    break;
+
+                case TouchActionType.Moved:
+                    TouchManipulationInfo info = touchDictionary[id];
+                    info.NewPoint = location;
+                    Manipulate();
+                    info.PreviousPoint = info.NewPoint;
+                    break;
+
+                case TouchActionType.Released:
+                    touchDictionary[id].NewPoint = location;
+                    Manipulate();
+                    touchDictionary.Remove(id);
+                    break;
+
+                case TouchActionType.Cancelled:
+                    touchDictionary.Remove(id);
+                    break;
+            }
+        }
+
+        // Enable anisotropic scaling
+        // Enable two-finger rotate
+        // Enable one-finger rotate
+
+
+        // Touch Manipulation Manager
+
+            // Enable properties
+
+            // SKMatrix OneTouch(SKPoint prevPoint, SKPoint newPoint, SKPoint pivot)
+
+            // SKMatrix TwoTouch(SKPoint prevPoint, SKPoint newPoint, SKPoint pivot)
+
+
+
+
+
+
+        void Manipulate()
+        {
+            if (touchDictionary.Count == 1)
+            {
+                TouchManipulationInfo info = touchDictionary.Values.First();
+                float xTranslate = info.NewPoint.X - info.PreviousPoint.X;
+                float yTranslate = info.NewPoint.Y - info.PreviousPoint.Y;
+
+                SKMatrix matrix = Matrix;
+                SKMatrix.PostConcat(ref matrix, SKMatrix.MakeTranslation(xTranslate, yTranslate));
+                Matrix = matrix;
+            }
+
+            else if (touchDictionary.Count >= 2)
+            {
+                TouchManipulationInfo[] infos = // new TouchManipulationInfo[touchDictionary.Count];
+                touchDictionary.Values.ToArray(); // .CopyTo(infos, 0);
+                int pivotIndex = infos[0].NewPoint == infos[0].PreviousPoint ? 0 : 1;
+
+                SKPoint pivot = infos[pivotIndex].NewPoint;
+                SKPoint newPoint = infos[1 - pivotIndex].NewPoint;
+                SKPoint prevPoint = infos[1 - pivotIndex].PreviousPoint;
+
+
+                // Anisotropic
+                /*
+                                float scaleX = (newPoint.X - pivot.X) / (prevPoint.X - pivot.X);
+                                float scaleY = (newPoint.Y - pivot.Y) / (prevPoint.Y - pivot.Y);
+                */
+
+                // Isotropic
+                float scale = Magnitude(newPoint - pivot) / Magnitude(prevPoint - pivot);
+                float scaleX = scale;
+                float scaleY = scale;
+
+
+
+                SKMatrix matrix = Matrix;
+                SKMatrix.PostConcat(ref matrix, SKMatrix.MakeScale(scaleX, scaleY, pivot.X, pivot.Y));
+                Matrix = matrix;
+            }
+        }
+
+        float Magnitude(SKPoint point)
+        {
+            return (float)Math.Sqrt(Math.Pow(point.X, 2) + Math.Pow(point.Y, 2));
+        }
+    }
+}
---
+++ SkiaSharpForms/SkiaSharpFormsDemos/SkiaSharpFormsDemos/SkiaSharpFormsDemos/Transforms/TouchManipulationPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+using Xamarin.Forms;
+
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
+using TouchTracking;
+
+namespace SkiaSharpFormsDemos.Transforms
+{
+    public partial class TouchManipulationPage : ContentPage
+    {
+        List<TouchManipulationBitmap> bitmaps = 
+            new List<TouchManipulationBitmap>();
+
+        Dictionary<long, TouchManipulationBitmap> bitmapDictionary = 
+            new Dictionary<long, TouchManipulationBitmap>();
+
+        public TouchManipulationPage()
+        {
+            InitializeComponent();
+
+            string resourceID = "SkiaSharpFormsDemos.Media.monkey.png";
+            Assembly assembly = GetType().GetTypeInfo().Assembly;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceID))
+            using (SKManagedStream skStream = new SKManagedStream(stream))
+            {
+                SKBitmap bitmap = SKBitmap.Decode(skStream);
+                bitmaps.Add(new TouchManipulationBitmap(bitmap)
+                {
+                    Matrix = SKMatrix.MakeScale(3, 3)
+                });
+            }
+        }
+
+        void OnTouchEffectAction(object sender, TouchActionEventArgs args)
+        {
+            SKPoint point = ConvertToPixel(args.Location);
+
+            switch (args.Type)
+            {
+                case TouchActionType.Pressed:
+                    foreach (TouchManipulationBitmap bitmap in bitmaps)
+                    {
+                        if (bitmap.HitTest(point))
+                        {
+                            bitmapDictionary.Add(args.Id, bitmap);
+                            bitmap.ProcessTouchEvent(args.Id, args.Type, point);
+                            break;
+                        }
+                    }
+                    break;
+
+                case TouchActionType.Moved:
+                    if (bitmapDictionary.ContainsKey(args.Id))
+                    {
+                        TouchManipulationBitmap bitmap = bitmapDictionary[args.Id];
+                        bitmap.ProcessTouchEvent(args.Id, args.Type, point);
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+
+                case TouchActionType.Released:
+                case TouchActionType.Cancelled:
+                    if (bitmapDictionary.ContainsKey(args.Id))
+                    {
+                        TouchManipulationBitmap bitmap = bitmapDictionary[args.Id];
+                        bitmap.ProcessTouchEvent(args.Id, args.Type, point);
+                        bitmapDictionary.Remove(args.Id);
+                        canvasView.InvalidateSurface();
+                    }
+                    break;
+            }
+        }
+
+        void OnCanvasViewPaintSurface(object sender, SKPaintSurfaceEventArgs args)
+        {
+            SKCanvas canvas = args.Surface.Canvas;
+            canvas.Clear();
+
+            foreach (TouchManipulationBitmap bitmap in bitmaps)
+            {
+                bitmap.Paint(canvas);
+            }
+        }
+
+        SKPoint ConvertToPixel(Point pt)
+        {
+            return new SKPoint((float)(canvasView.CanvasSize.Width * pt.X / canvasView.Width),
+                               (float)(canvasView.CanvasSize.Height * pt.Y / canvasView.Height));
+        }
+    }
+}
